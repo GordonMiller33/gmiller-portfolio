0c4e40a68d5a8085f7c15b237359a1936bc87791
GordonMiller33 <gordonmiller33@gmail.com>
2023-10-26 16:04:44 -0400

    Setup
    Initial Setup

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const projects = [
+    {
+      title: "Project #1",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "Project #2",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "Project #3",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Project #4",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+    "Javascript",
+    "React",
+    "Jambalaya",
+  ];
