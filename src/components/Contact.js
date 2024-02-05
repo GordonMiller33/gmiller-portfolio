4097a32a3a552282696a9505eea8d42560a47b76
GordonMiller33 <gordonmiller33@gmail.com>
2024-02-05 12:18:21 -0500

    more small changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,15 +38,14 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=208+West+Wilmont+Avenue,+Somers+Point,+NJ,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed/v1/place?q=Somers+Point,+NJ,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
+                LOCATION
               </h2>
               <p className="mt-1">
-                208 W Wilmont Ave. <br />
                 Somers Point, NJ 08244
               </p>
             </div>
